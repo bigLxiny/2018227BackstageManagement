5574266e4393cd0a8100227815cc9daacd54c71c
Lxy13755 <455837367@qq.com>
2018-03-05 11:17:33 +0800

    导航卫士（登录验证）

--- src/router/index.js
+++ src/router/index.js
@@ -19,9 +19,40 @@ const goods=[
     { name: 'goodsDetail', path: 'goods/detail/:id', component: GoodsDetail } 
 ]
 // 导出路由实例 
-export default new VueRouter({
+let router= new VueRouter({
     routes: [
         { name: 'login', path: '/login', component: Login },
         { name: 'admin', path: '/admin', component: Admin, children: [...goods] },
     ]
 })
+
+// 添加导航守卫   是一个函数
+router.beforeEach((to, from, next) => {
+    // 现在我们是在一个js模块当中, 并不是在vue组件中, 所以这里的this不是组件实例, 而是undefined, 无法this.$http与this.$api
+    // 我们可以在Vue的prototype对象上找$http和$api方法
+    Vue.prototype.$http.get(Vue.prototype.$api.islogin).then(res=>{
+        // 设置一个开关
+        let islogin=false;
+        // console.log(res);
+        if(res.data.code=='logined'){
+            islogin=true;
+        }
+        // 如果访问登陆页面
+        if(to.name=='login'){
+            if(islogin){
+                next({name:'admin'})
+            }else{
+                next()
+            }
+        }
+        // 如果访问其他后台页面
+        if(to.name!='login'){
+            if(islogin){
+                next()
+            }else{
+                next({name:'login'})
+            }
+        }
+    })
+})
+export default router;
